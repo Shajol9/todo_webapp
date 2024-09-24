5e2fd40429d6b5e248fa548aa4ef772efce33656
Mohammad Shazzad Hossain <89863207+Shajol9@users.noreply.github.com>
2024-09-24 12:59:51 +0200

    added add button for adding todo

--- todo_webapp.py
+++ todo_webapp.py
@@ -6,6 +6,7 @@ todo_list = functions.get_todo()
 
 def add_todo():
     newtodo = st.session_state["new_todo"] + "\n"
+    # if newtodo not in todo_list:
     todo_list.append(newtodo)
     functions.write_todo(todo_list)
     st.session_state["new_todo"] = ""
@@ -18,13 +19,17 @@ st.write('''You can add a new todo by typing it in the text box and
          deleted.''')
 
 for index, todo in enumerate(todo_list):
-    checkbox = st.checkbox(todo, key=todo)
+    checkbox = st.checkbox(todo, key=index)
     if checkbox:
         completed_item =  todo_list.pop(index)
         functions.write_todo(todo_list)
-        del st.session_state[todo]
+        del st.session_state[index]
         st.rerun()
 
 st.text_input(label ="Add new todo:", 
               placeholder="Type here to add nwe todo...",
-              on_change=add_todo, key="new_todo")
+              key="new_todo")
+
+st.button(label="Add" ,on_click=add_todo, key="add")
+
+#st.session_state
